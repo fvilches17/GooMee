f5fa4cc3203452a60790ffb74c50d4c0d4a6234e
Francisco Vilches <f_vilches17@outlook.com>
2021-04-30 22:39:24 +1200

    MVP complete. Added generate guid and copy guid functionality to UI

---
+++ src/GooMee.PWA/wwwroot/js/clipboard.js
@@ -0,0 +1,7 @@
+﻿export async function copy(text) {
+    try {
+        await navigator.clipboard.writeText(text);
+    } catch (error) {
+        console.error(`Could not copy text to clipboard: ${error}`);
+    }
+}
